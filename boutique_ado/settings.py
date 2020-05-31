461066b76db4493d0029c2e1d883f5ef73974693
Johann Alberts <alberts.johann.com>
2020-05-31 09:56:45 +0100

    Added mobile header and main navbar

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
+            os.path.join(BASE_DIR, 'templates', 'includes'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
