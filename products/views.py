3a2aa2e921cfeb50d183b6f35acc96c3120233a6
Johann Alberts <alberts.johann.com>
2020-06-01 20:59:17 +0100

    Added sorting JS and back to top link

--- products/views.py
+++ products/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from .models import Product, Category
 from django.db.models import Q
+from django.db.models.functions import Lower
+
+from .models import Product, Category
+
+# Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -19,7 +23,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -53,12 +58,12 @@ def all_products(request):
 
 
 def product_detail(request, product_id):
-    """A view to show individual product details"""
+    """ A view to show individual product details """
 
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
     }
-    
+
     return render(request, 'products/product_detail.html', context)
