386dc549c56dedcadaa14381a3842a6e8be068af
Johann Alberts <alberts.johann.com>
2020-06-07 12:11:57 +0000

    Added prfiles app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
@@ -176,6 +177,6 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Stripe
 STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY','')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY','')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY','')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY','')
+STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
